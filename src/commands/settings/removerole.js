8e20aec2fe8909b5e68af3ac541e83250c168ecf
artpumpkin <pumpkin.salah@gmail.com>
2020-10-12 15:39:25 +0100

    Add removerole command

---
+++ src/commands/settings/removerole.js
@@ -0,0 +1,53 @@
+const { embedMessage, addS, isAdmin } = require('../../utils/helpers');
+const Role = require('../../models/role');
+
+module.exports = {
+  name: 'removerole',
+  aliases: ['remove'],
+  cooldown: 3,
+  usage: 'removerole [role(s)]',
+  examples: ['removerole @staff', 'removerole @helper @crew'],
+  description: 'Remove a specific role limitation.',
+  guildOnly: true,
+  async execute(message, prefix, args) {
+    const messageEmbed = embedMessage(message);
+
+    if (!isAdmin(message.member)) {
+      messageEmbed.setDescription(
+        '> This command require `Administrator` permission.\n⠀',
+      );
+    } else if (args.length === 0) {
+      messageEmbed.setDescription(
+        "> You didn't specify a role or roles to remove.\n⠀",
+      );
+    } else {
+      const removedRoles = await Promise.all(
+        message.mentions.roles.map(async (role) => {
+          await Role.updateOne(
+            { guildID: role.guild.id },
+            {
+              guildID: role.guild.id,
+              $pull: {
+                roles: role.id,
+              },
+            },
+          );
+
+          await Role.deleteOne({ roles: [] });
+          return `<@&${role.id}>`;
+        }),
+      );
+
+      if (removedRoles.length > 0) {
+        messageEmbed.setDescription(
+          `> Role${addS(removedRoles.length)} ${removedRoles.join`, `} ${
+            removedRoles.length === 1 ? 'has' : 'have'
+          } been removed.`,
+        );
+      } else {
+        messageEmbed.setDescription('> No mentioned roles to remove.\n⠀');
+      }
+    }
+    return message.channel.send(messageEmbed);
+  },
+};
