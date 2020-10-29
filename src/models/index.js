d7294a1abcfd9ed7199c90157cfd79203cb1c744
artpumpkin <pumpkin.salah@gmail.com>
2020-10-29 08:33:38 +0100

    Refactor model functions

---
+++ src/models/index.js
@@ -0,0 +1,13 @@
+const Ban = require('./ban');
+const Grant = require('./grant');
+const Joindate = require('./joindate');
+const Prefix = require('./prefix');
+const Role = require('./role');
+
+module.exports = {
+  Ban,
+  Grant,
+  Joindate,
+  Prefix,
+  Role,
+};
